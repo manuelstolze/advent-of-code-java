34d2cb2bcfc2c727a848df724b07435695c0c862
Manuel Stolze <manuel.stolze93@hotmail.com>
2024-12-01 13:15:24 +0100

    feat: add solution for day 01

---
+++ src/test/java/year2024/day01/RiddleResolverTest.java
@@ -0,0 +1,78 @@
+package year2024.day01;
+
+import com.adventofcode.year2024.day01.RiddleResolver;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RiddleResolverTest {
+
+  RiddleResolver riddleResolver = new RiddleResolver();
+
+  @Test
+    void testReadFileCorrectly() {
+
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+
+    RiddleResolver.readFile("src/test/resources/2024/01_01.txt", list1, list2);
+
+    assert list1.size() == 6;
+    assert list2.size() == 6;
+  }
+
+  @Test
+  void testSortList() {
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+
+    RiddleResolver.readFile("src/test/resources/2024/01_01.txt", list1, list2);
+
+    Collections.sort(list1);
+    Collections.sort(list2);
+
+    assert list1.get(0) == 1;
+    assert list1.get(1) == 2;
+    assert list1.get(2) == 3;
+    assert list1.get(3) == 3;
+    assert list1.get(4) == 3;
+    assert list1.get(5) == 4;
+
+    assert list2.get(0) == 3;
+    assert list2.get(1) == 3;
+    assert list2.get(2) == 3;
+    assert list2.get(3) == 4;
+    assert list2.get(4) == 5;
+    assert list2.get(5) == 9;
+  }
+
+  @Test
+  void testCalculateTotalDistance(){
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+
+    RiddleResolver.readFile("src/test/resources/2024/01_01.txt", list1, list2);
+
+    Collections.sort(list1);
+    Collections.sort(list2);
+
+    int totalDistance = riddleResolver.calculateTotalDistance(list1, list2);
+    assert totalDistance == 11;
+  }
+
+  @Test
+  void testCalculateTotalDistanceWithSimilarityScore() {
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+
+    RiddleResolver.readFile("src/test/resources/2024/01_01.txt", list1, list2);
+
+    Collections.sort(list1);
+    Collections.sort(list2);
+
+    int totalDistance = riddleResolver.calculateTotalDistanceWithSimilarityScore(list1, list2);
+    assert totalDistance == 31;
+  }
+}
