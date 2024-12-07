58bbe95002145d739d21f73b85f92ad4549eb4f9
Manuel Stolze <manu.stolze93@yahoo.de>
2024-12-07 11:16:39 +0100

    feat: solve day 07

---
+++ src/main/java/com/adventofcode/year2024/day07/Day07RiddleRunner.java
@@ -0,0 +1,18 @@
+package com.adventofcode.year2024.day07;
+
+import com.adventofcode.common.AdventOfCodeFileReader;
+
+import java.util.List;
+
+public class Day07RiddleRunner {
+
+  public static void main(String[] args) {
+    Day07RiddleSolver riddle = new Day07RiddleSolver();
+    AdventOfCodeFileReader fileReader = new AdventOfCodeFileReader();
+
+    List<String> input =fileReader.readFile("src/main/resources/2024/07.txt");
+
+    System.out.println("Answer One: " + riddle.solveRiddleOne(input));
+    System.out.println("Answer Two: " + riddle.solveRiddleTwo(input));
+  }
+}
